c14a88e22890876bcc3083a602bb1199dd7b6ed7
Raoul Schorer <raoul@desktop-pc.home>
2020-08-02 19:45:09 +0200

    new package

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "test")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/test.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(raoul))
